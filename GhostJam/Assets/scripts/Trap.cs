98935c7bb9b415df9a38fc2ff06482cf3d1eaa8f
k.sekin <k.sekin@gambio.de>
2019-01-25 20:10:58 +0100

    Added trap logic and action script

--- GhostJam/Assets/scripts/Trap.cs
+++ GhostJam/Assets/scripts/Trap.cs
@@ -4,6 +4,25 @@ using UnityEngine;
 
 public class Trap : MonoBehaviour
 {
+    public ActionScript actionScript;
+
+    private BoxCollider2D collider;
+
+    void Awake() {
+        collider = gameObject.GetComponent(typeof(BoxCollider2D)) as BoxCollider2D;
+
+        if (collider == null) {
+            Debug.LogError("A trap object must have a collider attached");
+        }    
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.gameObject.tag == "Player") {
+            Debug.Log("The player entered a trap, this is very bad");
+            actionScript.performAction(col.gameObject);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
