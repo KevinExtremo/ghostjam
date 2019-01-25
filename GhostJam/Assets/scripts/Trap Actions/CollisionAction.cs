c48b5932d95ba605c2885a0bd27577fe8c361977
k.sekin <k.sekin@gambio.de>
2019-01-25 22:54:10 +0100

    Implemented lights to fall from the sky

---
+++ GhostJam/Assets/scripts/Trap Actions/CollisionAction.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionAction : ActionScript
+{
+    public void performAction(GameObject gm) {
+        
+    }
+}
