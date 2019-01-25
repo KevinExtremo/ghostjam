069405f251d69cfdb1ef30d8570b82f0e29594e2
Johannes Ganser <johannes.ganser@ubimax.com>
2019-01-26 00:56:08 +0100

    Camera is focusing on the player now

---
+++ GhostJam/Assets/scripts/CameraPlayerFocus.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPlayerFocus : MonoBehaviour
+{
+    [Tooltip("The transform of the player that shall be in focus")]
+    public Transform PlayerTransform;
+    [Tooltip("The position Offset of the Camera to the Player in Focus")]
+    public Vector3 Offset = Vector3.zero;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(PlayerTransform == null)
+        {
+            enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = PlayerTransform.position + Offset;
+    }
+}
