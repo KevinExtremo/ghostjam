072842de24ce20a814fb7250b5d17c2361dc84d9
Johannes Ganser <johannes.ganser@ubimax.com>
2019-01-27 15:48:52 +0100

    Cheststuff

---
+++ GhostJam/Assets/scripts/ChestStory.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestStory : MonoBehaviour
+{
+    public StoryItem closed;
+    public StoryItem open;
+    public GameObject Ghost;
+    public Chest chest;
+    public StoryItem key;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            if(!key.gameObject.activeSelf)
+            {
+                collision.GetComponent<PlayerPlatformerController>().StoryTextBoxController.SetNewStoryItem(closed);
+            }
+            else
+            {
+                collision.GetComponent<PlayerPlatformerController>().StoryTextBoxController.SetNewStoryItem(open);
+                Ghost.SetActive(true);
+            }
+        }
+    }
+}
