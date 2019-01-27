ffada31e47ce76dac97519a9cb3b8695e80a45c5
k.sekin <k.sekin@gambio.de>
2019-01-27 11:25:46 +0100

    Broken water faucet added(for now)

---
+++ GhostJam/Assets/scripts/Trap Trigger/WasserhahnActivationTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WasserhahnActivationTrigger : MonoBehaviour
+{
+    private bool triggered = false;
+
+    public void Reset() {
+        triggered = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.gameObject.tag == "Player" && !triggered) {
+            triggered = true;
+            this.gameObject.transform.parent.GetComponent<Wasserhahn>().SetFaucetActive(true);
+        }
+    }
+}
