b8b4a4457f530b8fc448995eb0358de0250dd987
Johannes Ganser <johannes.ganser@ubimax.com>
2019-01-27 13:19:53 +0100

    StoryItems for all!!

---
+++ GhostJam/Assets/scripts/MainFuse.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainFuse : MonoBehaviour
+{
+    public List<TrapController> trapControllers;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            foreach (var trapController in trapControllers)
+            {
+                //TODO: disable trapController
+            }
+        }
+    }
+
+
+    // Update is called once per frame
+    void Reset()
+    {
+        foreach(var trapController in trapControllers)
+        {
+            //TODO: enable trapController
+        }
+    }
+}
