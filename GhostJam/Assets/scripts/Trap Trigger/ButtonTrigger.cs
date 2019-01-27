299fbd2d37e16ca95854c4f31490af297d6ed0de
k.sekin <k.sekin@gambio.de>
2019-01-27 14:54:11 +0100

    Added the button puzzle

---
+++ GhostJam/Assets/scripts/Trap Trigger/ButtonTrigger.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTrigger : MonoBehaviour
+{
+    public string buttonColor;
+
+    private bool active = false;
+    
+    private float lastTriggeredTime;
+
+    void Awake() {
+        lastTriggeredTime = Time.time;
+    }
+
+    void OnTriggerEnter2D(Collider2D col) {
+        if (col.gameObject.tag == "Player") {
+            active = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D col) {
+        if (col.gameObject.tag == "Player") {
+            active = false;
+        }
+    }
+
+    void Update() {
+        if (Time.time - lastTriggeredTime > 1 && active && Input.GetAxis("Use") > 0) {
+            lastTriggeredTime = Time.time;
+            this.gameObject.transform.parent.GetComponent<ButtonPuzzle>().PressButton(buttonColor);
+        }
+    }
+}
---
+++ GhostJam/Assets/scripts/Traps/ButtonPuzzle.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonPuzzle : TrapController
+{
+    public GameObject blueButton;
+    public GameObject redButton;
+    public GameObject greenButton;
+    public GameObject yellowButton;
+
+    private string previousButton;
+    private bool finished = false;
+
+    void Awake() {
+        Reset();
+    }
+
+    public void PressButton(string color) {
+        switch(color) {
+            case "Grün":
+                if (previousButton != "") {
+                    GameState.gameState = GameState.State.GameOver;
+                }
+                previousButton = "Grün";
+                break;
+            case "Blau":
+                if (previousButton != "Grün") {
+                    GameState.gameState = GameState.State.GameOver;
+                }
+                previousButton = "Blau";
+                break;
+            case "Gelb":
+                if (previousButton != "Gelb") {
+                    GameState.gameState = GameState.State.GameOver;
+                }
+                previousButton = "Gelb";
+                break;
+            case "Rot":
+                if (previousButton != "Gelb") {
+                    GameState.gameState = GameState.State.GameOver;
+                }
+                previousButton = "Rot";
+                finished = true;
+                break;
+        }
+    }
+
+    public bool isFinished() {
+        return finished;
+    }
+
+    public override void Reset() {
+        previousButton = "";
+        finished = false;
+    }
+}
