281263c07be23e2b40feb92ec7c0df4bd44dfba2
k.sekin <k.sekin@gambio.de>
2019-01-27 14:13:46 +0100

    Added washing machine trap

---
+++ GhostJam/Assets/scripts/Traps/Waschmaschine.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waschmaschine : TrapController
+{
+    public override void Reset() {
+        this.gameObject.GetComponentInChildren<BoxCollider2D>().SetActive(true);
+    }
+}
